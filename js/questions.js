865e3aff0b31322d2c8b67aa2d6512ca71317ff4
alvinadeebhanafie <karenindah321@gmail.com>
2024-11-29 22:44:39 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Apa tujuan utama Gerakan 30 September (G30S/PKI)?",
+    answer: "c. Menggulingkan pemerintahan dan mendirikan negara komunis",
+    options: [
+      "a. Mempertahankan ideologi Pancasila",
+      "b. Menghapuskan sistem kolonial di Indonesia",
+      "c. Menggulingkan pemerintahan dan mendirikan negara komunis",
+      "d. Membentuk pemerintahan parlementer"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Siapa yang dianggap sebagai dalang utama dari peristiwa G30S/PKI?",
+    answer: "b. DN Aidit",
+    options: [
+      "a. Soekarno",
+      "b. DN Aidit",
+      "c. Suharto",
+      "d. Soeharto"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Berapa jumlah jenderal TNI AD yang menjadi korban dalam peristiwa G30S/PKI?",
+    answer: "a. 6 jenderal",
+    options: [
+      "a. 6 jenderal",
+      "b. 7 jenderal",
+      "c. 5 jenderal",
+      "d. 4 jenderal"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Dimana para korban peristiwa G30S/PKI ditemukan?",
+    answer: "d. Lubang Buaya",
+    options: [
+      "a. Monumen Nasional",
+      "b. Istana Negara",
+      "c. Markas PKI",
+      "d. Lubang Buaya"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Pada tahun berapa peristiwa G30S/PKI terjadi?",
+    answer: "c. 1965",
+    options: [
+      "a. 1960",
+      "b. 1963",
+      "c. 1965",
+      "d. 1970"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Apa nama organisasi militer yang dibentuk oleh PKI untuk mendukung rencananya?",
+    answer: "a. Barisan Tani Indonesia",
+    options: [
+      "a. Barisan Tani Indonesia",
+      "b. Angkatan Pemuda PKI",
+      "c. Milisi Rakyat",
+      "d. Pemuda Revolusioner"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Siapa presiden Indonesia saat peristiwa G30S/PKI terjadi?",
+    answer: "b. Soekarno",
+    options: [
+      "a. Suharto",
+      "b. Soekarno",
+      "c. Mohammad Hatta",
+      "d. BJ Habibie"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Apa nama dokumen yang diduga disusun untuk membenarkan aksi G30S/PKI?",
+    answer: "c. Dewan Jenderal",
+    options: [
+      "a. Dokumen Merah Putih",
+      "b. Rencana Revolusi",
+      "c. Dewan Jenderal",
+      "d. Operasi Militer Khusus"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Bagaimana sikap pemerintah setelah peristiwa G30S/PKI?",
+    answer: "a. PKI dibubarkan dan dinyatakan sebagai organisasi terlarang",
+    options: [
+      "a. PKI dibubarkan dan dinyatakan sebagai organisasi terlarang",
+      "b. Pemerintah berdamai dengan PKI",
+      "c. PKI diberi tempat di kabinet pemerintahan",
+      "d. PKI dilegalkan sebagai partai politik"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Apa nama monumen yang didirikan untuk mengenang korban G30S/PKI?",
+    answer: "d. Monumen Pancasila Sakti",
+    options: [
+      "a. Monumen Nasional",
+      "b. Monumen Proklamasi",
+      "c. Monumen Lubang Buaya",
+      "d. Monumen Pancasila Sakti"
+    ]
+  }
+]
